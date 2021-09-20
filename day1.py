c61931271a41ece72e3c6cb467a6e86a4ef501a7
DavitHoxton <72689635+DavitHoxton@users.noreply.github.com>
2021-09-20 14:48:48 +0400

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,129 @@
+#  1
+print('1 --- hello world')
+
+#  2
+print('2 --- davit khachiuri')
+
+#  3
+print('3 --- 128 sajaro')
+print('vajaze 4 kvartalshi')
+print('ar maxsovs')
+print('ar maxsovs')
+
+#  4
+print('4 --- ')
+print('hello world, hello world, hello world, hello world, hello world,')
+print('hello world, hello world, hello world, hello world,')
+print('hello world, hello world, hello world,')
+print('hello world, hello world,')
+print('hello world,')
+print('hello world, hello world,')
+print('hello world, hello world, hello world,')
+print('hello world, hello world, hello world, hello world,')
+print('hello world, hello world, hello world, hello world, hello world,')
+
+#  5
+print('5 --- \nC++\nC#\njava\npascal\nphp\nJavascript\nActionscript\Python')
+
+#  6
+x = 5
+y = 6
+
+print('6 --- x+y = ', x + y)
+print('6 --- x-y = ', x - y)
+print('6 --- x*y = ', x * y)
+
+#  7
+w = 15
+h = 16
+
+print('7 --- w+h = ', w + h)
+print('7 --- w-h = ', w - h)
+print('7 --- w*h = ', w * h)
+
+#  8
+k = 18
+z = 9
+
+print('8 --- k+z = ', k + z)
+print('8 --- k-z = ', k - z)
+print('8 --- k*z = ', k * z)
+
+#  9
+a = 23
+d = 24
+
+print('9 --- a+d = ', a + d)
+print('9 --- a-d = ', a - d)
+print('9 --- a*d = ', a * d)
+
+#  10
+a = 3
+b = 8
+c = 9
+
+jami = [a,b,c]
+print('10 --- a+b+c = ', a + b + c)
+print('10 --- abc = ', a * b * c)
+print('10 --- sashualo = ', int(sum(jami) / len(jami)))
+
+#  11
+x = 5
+y = 8
+z = 12
+j = 9
+
+if x == 5 and y == 8 and z == 12 and j == 9:
+    print('11 --- (x+y-(z*j)) = ', (x+y-(z*j)))
+elif x == -5 and y == 18 and z == -12 and j == 1:
+    print('11 --- (x/y+(z*j)) = ', (x/y+(z*j)))
+
+
+#  12
+a = 5
+b = 10
+
+#12.1
+print('12.1 --- 3a + 6 =   ',  3 * a + 6)
+#12.2
+print('12.2 --- 3a + 6b - 1 =   ',  3 * a + 6 * b - 1)
+#12.3
+k = 5
+t = 10
+z = 8
+print('12.3 --- k + t - z + 12 =   ', k + t - z + 12)
+
+#12.4
+a = 4
+t = 10
+print('12.4 --- (3a/4) + 6t =   ', (3 * a / 4) + (6 * t))
+
+#12.5
+h = 5
+d = 10
+
+print('12.5 --- -4h + 3d - 4 =   ', 4 * h + 3 * d - 4)
+
+#12.6
+a = 4
+b = 1
+
+print('12.6 --- (3a + 6) / (4 - 3b) =    ', int((3 * a + 6) / (4 - 3 * b)))
+
+#12.7
+a = 2
+c = 3
+
+print('12.7 --- 7a2 + 6c =    ', 7 * (a * a) + 6 * c)
+#12.8
+print('12.8 --- 2a2 + 6c3 =    ', 2 * (a * a) + 6 * (c * c * c))
+
+#12.9
+k = 2
+p = 3
+l = 4
+print('12.9 --- 11k + 2t - p + 12l =    ', 11 * k - p + 12 * l)
+
+#12.10
+m = 4
+print('12.10 --- k + (12l / 4m) =   ', int(k + (12 * l / (4 * m))))
